3c4009e075dce1f72b20b9c1fb222abaaff75776
Hawaiideveloper <coreymalbright@gmail.com>
2019-01-07 02:50:38 -0500

    tested and working

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+# if the variables file was enabled these would work
+  # access_key = "${var.aws_access_key}"
+  # secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
 
   version = "~> 1.7"
--- varibles.tf
+++ varibles.tf
@@ -2,13 +2,13 @@
 
 
 # Put your access_key in side the quotes
-#variable "aws_access_key" {
+# variable "aws_access_key" {
  # default = "access_key_here"
-#}
+# }
 
-#variable "aws_secret_key" {
+# variable "aws_secret_key" {
  # default = "put_your_40_character_secret_key_here"
-#}
+# }
 
 variable "aws_region" {
   default = "us-east-1"
