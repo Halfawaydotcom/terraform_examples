f681f9f002292767a751802882b3141500bb155c
Hawaiideveloper <coreymalbright@gmail.com>
2019-01-07 02:46:02 -0500

    using separate files

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-provider "aws" {
-  
-
-  version = "~> 1.7"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
@@ -28,4 +6,3 @@ resource "aws_instance" "my-test-instance" {
     Name = "test-instance"
   }
 }
-
